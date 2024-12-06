759dede8011003114be5747dbf7b92bf48334fec
MilosRist <milosristovic007@gmail.com>
2024-12-05 18:47:40 -0800

    fetching from deployed api instead of localhost

--- src/posts/ProblemSet1.jsx
+++ src/posts/ProblemSet1.jsx
@@ -9,7 +9,7 @@ const ProblemSet1 = () => {
         useEffect(() => {
             const fetchData = async () => {
                 try {
-                    const response = await fetch('http://localhost:3001/api/answers');
+                    const response = await fetch('https://victoria-chess-coaching-app.onrender.com/api/answers');
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
                     }
