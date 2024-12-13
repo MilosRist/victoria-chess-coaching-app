976c9b9d04d12d79df91635909ffed28e8f45a0b
MilosRist <milosristovic007@gmail.com>
2024-12-13 09:26:44 -0800

    more user auth

--- src/services/index.cjs
+++ src/services/index.cjs
@@ -61,8 +61,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join('dist', 'index.html'));
 });
 
-console.log(users)
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
